b91d30c6558b7229f9914875f7b03050f5d4a43f
Ilse Paniagua <ilsep@umich.edu>
2023-02-04 16:17:06 -0500

    rename deployment file to gcs_deployment

--- homework/workflow_orchestration/gcs_deployment.py
+++ homework/workflow_orchestration/gcs_deployment.py
@@ -1,12 +1,17 @@
 from etl_web_to_gcs import etl_parent_flow
+from prefect.blocks.core import Block
 from prefect.deployments import Deployment
+from prefect.filesystems import GitHub
+
+github_block = GitHub.load("github-repo")
 
 deployment = Deployment.build_from_flow(
     flow=etl_parent_flow,
-    name="multi_green_taxi_flow",
+    name="github_multi_green_taxi_flow",
     parameters={"color": "green", "year": 2020, "months": [1,2,3]},
     infra_overrides={"env": {"PREFECT_LOGGING_LEVEL": "DEBUG"}},
     work_queue_name="default",
+    storage=github_block
 )
 
 if __name__ == "__main__":
