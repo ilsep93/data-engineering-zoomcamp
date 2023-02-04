2662b5cadec5ae3106f75249de5f599cc408d502
Ilse Paniagua <ilsep@umich.edu>
2023-02-04 14:08:47 -0500

    python deployment initial commit

---
+++ homework/workflow_orchestration/deployment.py
@@ -0,0 +1,13 @@
+from etl_web_to_gcs import etl_web_to_gcs
+from prefect.deployments import Deployment
+
+deployment = Deployment.build_from_flow(
+    flow=etl_web_to_gcs,
+    name="green_taxi_flow",
+    parameters={"color": "green", "year": 2020, "months": [1,2,3]},
+    infra_overrides={"env": {"PREFECT_LOGGING_LEVEL": "DEBUG"}},
+    work_queue_name="default",
+)
+
+if __name__ == "__main__":
+    deployment.apply()
