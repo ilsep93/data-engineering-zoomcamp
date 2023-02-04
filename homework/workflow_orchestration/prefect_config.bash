5cf52177d0ce85613d0a1979e9983d31b9cedafa
Ilse Paniagua <ilsep@umich.edu>
2023-02-04 12:39:03 -0500

    move notes about the prefect api
    Lines should only be uncommented if there is trouble reaching the api.

--- homework/workflow_orchestration/prefect_config.bash
+++ homework/workflow_orchestration/prefect_config.bash
@@ -2,7 +2,6 @@
 
 #Launch the UI
 prefect orion start
-prefect config set PREFECT_API_URL="http://127.0.0.1:4200/api"
 
 #Check out the dashboard at http://127.0.0.1:4200
 
@@ -11,3 +10,6 @@ prefect block register -m prefect_gcp
 
 #To configure the newly registered blocks, go to the Blocks page in the Prefect UI: 
 #http://127.0.0.1:4200/blocks/catalog
+
+#prefect config set PREFECT_API_URL="http://127.0.0.1:4200/api"
+#prefect config unset PREFECT_API_URL
