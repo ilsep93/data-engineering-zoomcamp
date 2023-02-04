4e4503ca1c01c537032a02ae4aeb4b377e994d5e
Ilse Paniagua <ilsep@umich.edu>
2023-02-04 12:38:33 -0500

    edit name of gcs bucket

--- homework/workflow_orchestration/make_gcp_blocks.py
+++ homework/workflow_orchestration/make_gcp_blocks.py
@@ -2,6 +2,6 @@ from prefect_gcp import GcpCredentials
 from prefect_gcp.cloud_storage import GcsBucket
 
 gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
-gcp_cloud_storage_bucket_block = GcsBucket.load("green-taxi-bucket")
+gcp_bucket = GcsBucket.load("green-taxi-rides")
 
 #gcp_cloud_storage_bucket_block.save("zoom-gcs", overwrite=True)
