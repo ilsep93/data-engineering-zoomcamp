cd000accd7b0cdfd52590dec118f8a9a41e364b5
Ilse Paniagua <ilsep@umich.edu>
2023-02-04 19:18:29 -0500

    fix bug for making directories
    Parquet file was not being recognized as a file, but as a directory

--- homework/workflow_orchestration/etl_web_to_gcs.py
+++ homework/workflow_orchestration/etl_web_to_gcs.py
@@ -29,8 +29,12 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
-    path = Path(f"data/{color}/{dataset_file}.parquet").mkdir(parents=True, exist_ok=True)
+    dir = Path(f"data/{color}")
+    dir.mkdir(parents=True, exist_ok=True)
+    
+    path = Path(f"data/{color}/{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
+
     return path
 
 
